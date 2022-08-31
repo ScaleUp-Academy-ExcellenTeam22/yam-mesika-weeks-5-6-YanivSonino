bf0a39323c8a646102a4943a632afe916eb28f4b
YanivSonino <y1a2n3i4v55@gmail.com>
2022-08-31 23:33:04 +0300

    Code improvements.

--- DontHaveVinigrat.py
+++ DontHaveVinigrat.py
@@ -3,28 +3,31 @@ import random
 
 
 def random_date():
+    """
+
+    :return:
+    """
     first_date = input("Enter first date in format YYYY-MM-DD: ")
     second_date = input("Enter second date in format YYYY-MM-DD: ")
     try:
         first_date = datetime.date.fromisoformat(first_date)
         second_date = datetime.date.fromisoformat(second_date)
     except ValueError:
-        print("date is not as the format described")
+        print("Date is not as the format described")
         return
     if first_date > second_date:
-        Timedelta = first_date - second_date
+        time_delta = first_date - second_date
     else:
-        Timedelta = second_date - first_date
-
-    random_date_for_vini = datetime.date
+        time_delta = second_date - first_date
 
     if first_date > second_date:
-        random_date_for_vini = first_date + datetime.timedelta(days=random.randrange(Timedelta.days))
+        random_date_for_vini = first_date + datetime.timedelta(days=random.randrange(time_delta.days))
     else:
-        random_date_for_vini = second_date + datetime.timedelta(days=random.randrange(Timedelta.days))
+        random_date_for_vini = second_date + datetime.timedelta(days=random.randrange(time_delta.days))
 
     if random_date_for_vini.weekday() == 0:
         print("I dont have any vinigrat")
 
 
-random_date()
+if __name__ == '__main__':
+    random_date()
--- ThatsTheWay.py
+++ ThatsTheWay.py
@@ -1,15 +1,19 @@
 import os
+from typing import List, Tuple, Any
 
 
-def find_deep(path):
-    file = os.listdir(path)
-    files_with_deep = []
-    for name in file:
-        if str(name).startswith("deep"):
-            files_with_deep.append(name)
-    return files_with_deep
+def find_deep(path: str) -> List[str] | Tuple[Any, ...]:
+    """
+    Find files that start with the word deep in their title.
+    :param path: root to directory that contain files.
+    :return: list of files.
+    """
+    try:
+        file = os.listdir(path)
+        return [name for name in file if str(name).startswith("deep")]
+    except FileNotFoundError as e:
+        return e.args
 
 
-print(find_deep('C:\\Users\\USER\\gitHub\\Notebooks1\\week05\\images'))
-
-
+if __name__ == '__main__':
+    print(find_deep('C:\\Users\\USER\\gitHub\\Notebooks1\\week05\\images'))
