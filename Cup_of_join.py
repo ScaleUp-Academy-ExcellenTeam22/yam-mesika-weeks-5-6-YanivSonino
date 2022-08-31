77773e5dca82f7ac593203c6e8416908be973de9
YanivSonino <y1a2n3i4v55@gmail.com>
2022-09-01 00:01:58 +0300

    Code improvements.

--- Cup_of_join.py
+++ Cup_of_join.py
@@ -1,20 +1,25 @@
+from typing import List
 
 
-def join(*lists, sep='-'):
-    if not lists:
-        return None
+def join(*lists, sep='-') -> List:
+    """
+    Join lists with seperator.
+    :param lists: lists of lists
+    :param sep: seperator operator between list.
+    :return: join list.
+    """
 
     list_of_all = []
-    for lst in lists:
-        for item in lst:
-            list_of_all.append(item)
-        if len(lists) > 1:  # to separate lists
+    for index, lst in enumerate(lists):
+        list_of_all += [item for item in lst]
+        if len(lists) > 1 and index < len(lists) - 1:  # to separate lists
             list_of_all.append(sep)
     return list_of_all
 
 
 # Tests
-print(join([1, 2], [8], [9, 5, 6], sep='@'))
-print(([1, 2], [8], [9, 5, 6]))
-print(join([1]))
-print(join())
+if __name__ == '__main__':
+    print(join([1, 2], [8], [9, 5, 6], sep='@'))
+    print(join([1, 2], [8], [9, 5, 6]))
+    print(join([1]))
+    print(join())
