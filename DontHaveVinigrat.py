da221fcd7b52aa135169a5d3f2b22c58ea31201d
YanivSonino <y1a2n3i4v55@gmail.com>
2022-09-02 11:34:19 +0300

    Seperate IO from logic

--- DontHaveVinigrat.py
+++ DontHaveVinigrat.py
@@ -1,32 +1,35 @@
 import datetime
 import random
+from xmlrpc.client import Boolean
 
 
 def random_date():
-    """
-
-    :return:
-    """
     first_date = input("Enter first date in format YYYY-MM-DD: ")
     second_date = input("Enter second date in format YYYY-MM-DD: ")
     try:
         first_date = datetime.date.fromisoformat(first_date)
         second_date = datetime.date.fromisoformat(second_date)
-    except ValueError:
-        print("Date is not as the format described")
+    except ValueError as e:
+        print(f"{e.args} Date is not as the format described")
         return
-    if first_date > second_date:
-        time_delta = first_date - second_date
-    else:
-        time_delta = second_date - first_date
 
+    if check_if_date_on_day(0, first_date, second_date):
+        print("I dont have any vinigrat")
+
+
+def check_if_date_on_day(day, first_date, second_date) -> Boolean:
+    """
+    :param day: The day to check
+    :param first_date: First date input.
+    :param second_date: Second date input.
+    :return: Boolean that indicate if the random date is on the chosen day.
+    """
+    time_delta = abs(first_date - second_date)
     if first_date > second_date:
         random_date_for_vini = first_date + datetime.timedelta(days=random.randrange(time_delta.days))
     else:
         random_date_for_vini = second_date + datetime.timedelta(days=random.randrange(time_delta.days))
-
-    if random_date_for_vini.weekday() == 0:
-        print("I dont have any vinigrat")
+    return random_date_for_vini.weekday() == day
 
 
 if __name__ == '__main__':
