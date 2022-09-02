8f053fe7a06d67fc782eafafad7fc3623e7119ef
YanivSonino <y1a2n3i4v55@gmail.com>
2022-09-02 11:41:04 +0300

    Avoid Abreverssion variable.

--- Cup_of_join.py
+++ Cup_of_join.py
@@ -1,11 +1,11 @@
 from typing import List
 
 
-def join(*lists, sep='-') -> List:
+def join(*lists, seperator_character='-') -> List:
     """
     Join lists with seperator.
+    :param seperator_character: seperator operator between list.
     :param lists: lists of lists
-    :param sep: seperator operator between list.
     :return: join list.
     """
 
@@ -13,13 +13,12 @@ def join(*lists, sep='-') -> List:
     for index, lst in enumerate(lists):
         list_of_all += [item for item in lst]
         if len(lists) > 1 and index < len(lists) - 1:  # to separate lists
-            list_of_all.append(sep)
+            list_of_all.append(seperator_character)
     return list_of_all
 
 
-# Tests
 if __name__ == '__main__':
-    print(join([1, 2], [8], [9, 5, 6], sep='@'))
+    print(join([1, 2], [8], [9, 5, 6], seperator_character='@'))
     print(join([1, 2], [8], [9, 5, 6]))
     print(join([1]))
     print(join())
